0c91a92a635b3ea8a14c897c6affd29d34ea13aa
Giovanni Moratto <morattotrabalho@gmail.com>
2022-04-15 23:29:05 -0300

    initial setting

---
+++ src/main/java/br/com/giovannimoratto/codelibraryapi/CodeLibraryApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.giovannimoratto.codelibraryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeLibraryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeLibraryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/giovannimoratto/codelibraryapi/CodeLibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.giovannimoratto.codelibraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeLibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
