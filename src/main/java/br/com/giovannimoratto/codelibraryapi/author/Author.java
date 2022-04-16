d5f4b0838a304c732fb1513891d3855b3d0525bb
Giovanni Moratto <morattotrabalho@gmail.com>
2022-04-16 00:54:03 -0300

    creating author feature

---
+++ src/main/java/br/com/giovannimoratto/codelibraryapi/author/Author.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2022. Giovanni Vicentin Moratto - All rights reserved.
+ * You may use, distribute and modify this code under the
+ * terms of the MIT license, which unfortunately won't be
+ * written for another century.
+ *
+ * You should have received a copy of the MIT license with
+ * this file. If not, please write to: morattotrabalho@gmail.com, or visit : https://www.linkedin.com/in/giovannimoratto/
+ */
+
+package br.com.giovannimoratto.codelibraryapi.author;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "`TB_AUTHORS`")
+public class Author {
+
+    /* Properties */
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false, length = 400)
+    private String description;
+    @CreationTimestamp
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    /* Constructors */
+
+    @Deprecated
+    public Author() {
+    }
+
+    public Author(String name, String email, String description) {
+        this.name = name;
+        this.email = email;
+        this.description = description;
+    }
+
+    /* Getters */
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+}
---
+++ src/main/java/br/com/giovannimoratto/codelibraryapi/author/AuthorRequest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2022. Giovanni Vicentin Moratto - All rights reserved.
+ * You may use, distribute and modify this code under the
+ * terms of the MIT license, which unfortunately won't be
+ * written for another century.
+ *
+ * You should have received a copy of the MIT license with
+ * this file. If not, please write to: morattotrabalho@gmail.com, or visit : https://www.linkedin.com/in/giovannimoratto/
+ */
+
+package br.com.giovannimoratto.codelibraryapi.author;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class AuthorRequest {
+
+    /* Properties */
+    @NotBlank
+    private final String name;
+    @NotBlank
+    @Email
+    private final String email;
+    @NotBlank
+    @Size(max = 400)
+    private final String description;
+
+    /* Constructors */
+    public AuthorRequest(String name, String email, String description) {
+        this.name = name;
+        this.email = email;
+        this.description = description;
+    }
+
+    /* Methods */
+    public Author toModel() {
+        return new Author(name, email, description);
+    }
+
+}
